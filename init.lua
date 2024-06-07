c2f291d153a710f7f46834ead8cfdaef7424db85
Per Malmberg <per.malmberg@gmail.com>
2024-06-07 20:10:02 +0200

    Add Screekey with binding <leader>uk.

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
-vim.o.shell = "pwsh"
-vim.o.shellcmdflag =
-	"-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+if vim.fn.has("win32") then
+	vim.o.shell = "pwsh"
+	vim.o.shellcmdflag =
+		"-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+	vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+	vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+end
+
 vim.o.shellquote = ""
 vim.o.shellxquote = ""
 -- bootstrap lazy.nvim, LazyVim and your plugins
---
+++ lua/plugins/screenkey.lua
@@ -0,0 +1,17 @@
+return {
+	"NStefan002/screenkey.nvim",
+	dependencies = {
+		"folke/which-key.nvim",
+	},
+	cmd = "Screenkey",
+	version = "*",
+	config = function()
+		local wk = require("which-key")
+
+		wk.register({
+			["<leader>u"] = {
+				k = { "<cmd>Screenkey<cr>", "Toggle Screenkey" },
+			},
+		})
+	end,
+}

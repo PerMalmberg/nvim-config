bedb2acb303ea339c4edf0f5f735c6dd9981f495
Per Malmberg <per.malmberg@flexera.com>
2025-10-24 16:10:10 +0200

    colorscheme gruvdark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,6 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("onedark").setup({
-      style = "warmer",
-    })
-    -- Enable theme
-    require("onedark").load()
-  end,
+  "darianmorat/gruvdark.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
 }
